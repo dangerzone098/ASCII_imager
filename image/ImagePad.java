46638fb56f7fd4ce16b12e083520fe23f957e78d
adike <adi.keezl@gmail.com>
2026-05-30 17:28:02 +0300

    first commit

--- image/ImagePad.java
+++ image/ImagePad.java
@@ -8,25 +8,45 @@ public class ImagePad {
     private final Image image;
     private final Image paddedImage;
 
-    public record Dimensions(int height, int width) {}
+    public record Dimensions(int width, int height) {}
 
     public void pad(){
 
     }
 
     public ImagePad(Image image){
+        // TODO: test
+
         this.image = image;
 
         Dimensions newDimensions = getNewDimensions();
 
-        Color[][] paddedImageArray = new Color[][]();
-        this.paddedImage = new Image(newDimensions.height, newDimensions.width);
+        Color[][] paddedImageArray = new Color[newDimensions.height][newDimensions.width];
+        this.paddedImage = new Image(paddedImageArray, newDimensions.width, newDimensions.height);
     }
 
+    private boolean isPowOf2(int num){
+        return num > 0 && Integer.bitCount(num) == 1;
+    }
+
+    private int nextPowOf2(int num){
+        if (num <= 1){
+            return 1;
+        }
+        return Integer.highestOneBit(num - 1) << 1;
+    }
 
     private Dimensions getNewDimensions(){
-        int height, width;
-        return new Dimensions(height, width);
+        // TODO: test
+
+        int height = image.getHeight();
+        int width = image.getWidth();
+        if (isPowOf2(height) && isPowOf2(width)){
+            return new Dimensions(width, height);
+        }
+        else{
+            return new Dimensions(nextPowOf2(width), nextPowOf2(height));
+        }
     }
 
 
